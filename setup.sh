6edd37e2e1df5c15faec3b670d2ed3970a24c265
Ogasawara <tsutomu.ogasawara@P30476.local>
2015-03-23 16:55:01 +0900

    Added .bashrc_local.

--- setup.sh
+++ setup.sh
@@ -3,3 +3,5 @@
 cd $HOME
 ln -s $HOME/.dotfiles/.bash_profile
 ln -s $HOME/.dotfiles/.bashrc
+cp $HOME/.dotfiles/.bashrc_local.template $HOME/.dotfiles/.bashrc_local
+ln -s $HOME/.dotfiles/.bashrc_local
