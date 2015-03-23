327c5aafbe3616f0d4e91d04e2c5199c9bcd1319
Tsutomu Ogasawara <tsutomu.ogasawara@rakuten.com>
2015-03-23 16:57:26 +0900

    Added .inputrc.

--- setup.sh
+++ setup.sh
@@ -5,3 +5,4 @@ ln -s $HOME/.dotfiles/.bash_profile
 ln -s $HOME/.dotfiles/.bashrc
 cp $HOME/.dotfiles/.bashrc_local.template $HOME/.dotfiles/.bashrc_local
 ln -s $HOME/.dotfiles/.bashrc_local
+ln -s $HOME/.dotfiles/.inputrc
